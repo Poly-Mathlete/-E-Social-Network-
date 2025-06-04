0fc86cf43ec58e66e44221107f76ceb4b12fbbf4
Ilhan Amirou <ilhan.amirou@student-cs.fr>
2025-06-04 11:07:38 +0200

    main annoté

--- clean_Final.py
+++ clean_Final.py
@@ -1,9 +1,9 @@
-import re
-from pathlib import Path
+import re #pour utiliser expressions régulières
+from pathlib import Path #pour manipuler les fichiers
 
-import nltk
+import nltk #notre bibliothèque de traitement de texte documenté 
 from nltk.corpus import stopwords
-from nltk.tokenize import TreebankWordTokenizer
+from nltk.tokenize import TreebankWordTokenizer #tokenizer : transforme les phrases en liste de mots 
 
 # Télécharger les ressources NLTK
 nltk.download("punkt", quiet=True)
@@ -23,7 +23,7 @@ regex_artifacts_sources = re.compile(
 )
 
 # on supprime les noms d'utilisateurs mentionnés 
-regex_usernames = re.compile(r'@\w+')
+regex_usernames = re.compile(r'@\w+') 
 
 # on supprime les URL (http ou https)
 regex_urls = re.compile(r'https?://\S+')
@@ -54,7 +54,7 @@ for i in range(1, 5):
 
     cleaned_tweets = []
 
-    # Choix de la langue pour les stopwords
+    # Choix de la langue pour les stopwords selon le document
     langue = "french" if i in [1, 2] else "english"
     stop_words = set(stopwords.words(langue))
 
@@ -64,11 +64,11 @@ for i in range(1, 5):
     for line in lines:
         # Ignore les lignes vides ou invalides
         if line == '"","x"' or not line.strip():
-            continue
+            continue # On ignore le reste dans ce cas
         # Extraction du numéro de tweet et du texte via regex
         m = re.match(r'^"(\d+)","(.*)"$', line)
         if not m:
-            continue
+            continue # On ignore le reste dans ce cas
         tweet_num, tweet_text = m.groups()
 
         # Nettoyage du texte 
